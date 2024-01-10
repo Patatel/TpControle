6ed96923c1025036a963d25f3d5468f7214b2391
Patatel <lea.regoudis@gmail.com>
2024-01-10 16:44:37 +0100

    finfin

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -32,4 +32,46 @@ class FeedbackController extends AbstractController
         'feedbackform' => $form->createView(),
         ]);
     } 
+
+    #[Route('/feedbacks', name: 'feedback_list')]
+    public function list(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Feedback::class);
+        $feed = $repository->findAll();
+        return $this->render('feedback/list.html.twig', [
+            'feedbacks' => $feed,
+        ]);
+    }
+
+    #[Route('/feedback/edit/{id}', name: 'feedback_edit')]
+    public function edit(Request $request, ManagerRegistry $doctrine, $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $feed = $entityManager->getRepository(Feedback::class)->find($id);
+        if (!$feed) {
+            throw $this->createNotFoundException('No feedback found for id '.$id);
+        }
+        $feedForm = $this->createForm(FeedbackType::class, $feed);
+        $feedForm->handleRequest($request);
+        if ($feedForm->isSubmitted() && $feedForm->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('feedback_list');
+        }
+        return $this->render('feedback/edit.html.twig', [
+            'feedbackform' => $feedForm->createView(),
+        ]);
+    }
+
+    #[Route('/feedback/delete/{id}', name: 'feedback_delete')]
+    public function delete(ManagerRegistry $doctrine, $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $feed = $entityManager->getRepository(Feedback::class)->find($id);
+        if (!$feed) {
+            throw $this->createNotFoundException('No feedback found for id '.$id);
+        }
+        $entityManager->remove($feed);
+        $entityManager->flush();
+        return $this->redirectToRoute('feedback_list');
+    }
 }
